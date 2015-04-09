d46ee52a37d6924b2922b3c728234d414c196fa0
Neil Calabroso <nmcalabroso@up.edu.ph>
2015-04-09 09:02:54 +0800

    Disable update check for app engine in the automated script

--- run-ae.sh
+++ run-ae.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd appinventor/
 echo "Starting AppEngine..."
-dev_appserver.sh --port=8888 --address=0.0.0.0 appengine/build/war
+dev_appserver.sh --port=8888 --address=0.0.0.0 --disable_update_check appengine/build/war
 echo "AppEngine started!"
